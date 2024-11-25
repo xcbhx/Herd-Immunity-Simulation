8ef7b89662e49709b46ec25fa8414a44fb767dfc
xchbx <ceina.ellison@gmail.com>
2024-11-25 10:37:48 -0500

    edited virus.py

--- virus.py
+++ virus.py
@@ -4,8 +4,12 @@ class Virus(object):
         # Define the attributes of your your virus
         self.name = name
         # TODO Define the other attributes of Virus
-        pass
+        self.repro_rate = repro_rate
+        self.mortality_rate = mortality_rate
 
+    def __repr__(self):
+        # Return a string representation of the Virus object
+        return f'Virus (name = {self.name}, repro_rate = {self.repro_rate}, mortaility_rate = {self.mortality_rate})'
 
 # Test this class
 if __name__ == "__main__":
@@ -15,3 +19,14 @@ if __name__ == "__main__":
     assert virus.name == "HIV"
     assert virus.repro_rate == 0.8
     assert virus.mortality_rate == 0.3
+
+    virus_1 = Virus("E.Coli", 0.1, 0.015)
+    virus_2 = Virus("Tuberculosis", 0.055, 0.65)
+    virus_3 = Virus("Ebola", 0.023, 0.7)
+    virus_4 = Virus("Rabies",0.014, 1)
+
+    print(virus)
+    print(virus_1)
+    print(virus_2)
+    print(virus_3)
+    print(virus_4)
